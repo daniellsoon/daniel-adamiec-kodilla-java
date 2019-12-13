31cfdb24f7ff26bcaf8d58b550a542c0ec3ad200
daniellsoon <daniel.adamiec@hotmail.com>
2019-12-14 00:55:25 +0100

    first tests added

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,27 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
+public class TestingMain {
+    public static void main(String[] args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calc = new Calculator();
+
+        if (calc.add(5,5) == 10) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+
+        }
     }
 }
