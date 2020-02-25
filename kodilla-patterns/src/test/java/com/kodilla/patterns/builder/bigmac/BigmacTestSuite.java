31634039d9b1a5bd775ebab106179cde2ade44ca
daniellsoon <daniel.adamiec@hotmail.com>
2020-02-25 17:49:46 +0100

    15.5

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("with sesame")
+                .burgers(2)
+                .sauce("hot")
+                .ingredient("onion")
+                .ingredient("cucumber")
+                .ingredient("mushrooms")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(3, howManyIngredients);
+    }
+}
