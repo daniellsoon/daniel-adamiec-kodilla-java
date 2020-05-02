612c261b7cb3d20c58dc6c9949baa9c18d6bb90e
daniellsoon <daniel.adamiec@hotmail.com>
2020-05-02 17:33:26 +0200

    24.3. Aspects

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.swing.*;
 import java.math.BigDecimal;
 
 @RunWith(SpringRunner.class)
